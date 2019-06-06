e6fcf259ad8b1a085df49ea11d3940eb5fc71de3
dishaaa029 <51439033+dishaaa029@users.noreply.github.com>
2019-06-06 14:14:53 +0530

    Create dexter.js

---
+++ dexter.js
@@ -0,0 +1,41 @@
+<form name="F187" id="F187" onsubmit="MySearchF(); return false">
+
+<br />
+
+Enter a search word or phrase:
+
+<br />
+
+<input type="text" name="MyText" id="MyText"value="" size="27" maxlength="25" />
+
+<input type="button" value="Search" onclick="MySearchF()" />
+
+<br />
+
+<select name="MySelect" id="MySelect" style="width:250" size="5" ondblclick="searchGo()">
+
+<option>
+
+Your answer will appear here
+
+</select>
+
+<br />
+
+<input type="button" value="Go!" onclick="searchGo()}" />
+
+<br />
+
+Produced by
+
+<a href="https://trans4mind.com/personal_development/jsEditorTutorial/searchGenerator.htm">
+
+Search
+
+Engine Generator
+
+</a>
+
+.
+
+</form>
